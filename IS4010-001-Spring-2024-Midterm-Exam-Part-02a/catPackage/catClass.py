20b8a404ec1d72c0d0048d864daf060c1255cf18
aanikagarre <aanikagarre@aanikas-mbp.wifi.uc.edu>
2024-03-07 12:59:47 -0500

    First commit

--- IS4010-001-Spring-2024-Midterm-Exam-Part-02a/catPackage/catClass.py
+++ IS4010-001-Spring-2024-Midterm-Exam-Part-02a/catPackage/catClass.py
@@ -1,21 +1,29 @@
-
+#catClass.py
 
 # 1. Complete the sayMeow method
 # 2. Complete the getName method
+
 class Cat:
+    '''
+    Cat Class
+    This class models a pet cat's name and meow
+    '''
     def __init__(self, name):
+        '''
+        @param: name - name of the Cat
+        '''
         self.name = name
     
     def sayMeow(self):
         '''
         Print meow to the console
         '''
-        pass
+        print("meow")
     
     def getName(self):
         '''
         Get the name of the Cat object
         @return: The name of the Cat object
         '''
-        pass
+        return self.name
     
--- IS4010-001-Spring-2024-Midterm-Exam-Part-02a/mainPackage/main.py
+++ IS4010-001-Spring-2024-Midterm-Exam-Part-02a/mainPackage/main.py
@@ -1,3 +1,17 @@
+#main.py
+
+# Name: Aanika Garre
+# email: garreaa@mail.uc.edu
+# Assignment Number: Midterm Exam Part 2a
+# Due Date: 3/7/24
+# Course/Section: IS 4010-001
+# Semester/Year: Spring 2024
+# Brief Description of the assignment: This assignment focuses on testing our cumulative knowledge from the entire semester.
+
+# Brief Description: This module goes over a basic understanding of forking repositories, importing a project into
+# Eclipse, creating methods, instantiating objects, and more.
+# Citations:
+# Anything else that's relevant:
 
 from catPackage.catClass import Cat
 
@@ -10,4 +24,6 @@ if __name__ == "__main__":
     
     # 3. Instantiate a Cat object, demonstrate the sayMeow and the getName methods
 
-    
+    myKitten = Cat("Hollie")
+    myKitten.sayMeow()
+    print(myKitten.getName())
